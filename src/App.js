dc207bef83af4a0eeb3ed0684915df7dc595858d
rahuljha661 <rahuljha12527@gmail.com>
2020-10-09 23:47:31 +0530

    add useState

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
+import HookCounterThree from "./components/HookCounterThree";
 import HooksCounterTwo from "./components/HooksCounterTwo";
 
 function App() {
   return <div className="App">
     {/* <ClassCounter /> */}
     {/* <HookCounter /> */}
-    <HooksCounterTwo/>
+    {/* <HooksCounterTwo/> */}
+    <HookCounterThree />
   </div>;
 }
 
