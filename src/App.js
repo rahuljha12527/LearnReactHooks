5c956ea7f313535a9dc665e17d2df62068018209
rahuljha661 <rahuljha12527@gmail.com>
2020-10-10 14:42:54 +0530

    add reducer increment decremenet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
+import ComponentC from "./components/ComponentC";
 import DataFetching from "./components/DataFetching";
 import HookCounter from "./components/HookCounter";
 import HookCounterOne from "./components/HookCounterOne";
@@ -10,6 +11,11 @@ import HooksCounterFour from "./components/HooksCounterFour";
 import HooksCounterTwo from "./components/HooksCounterTwo";
 import IntervalHookCounter from "./components/IntervalHookCounter";
 import MouseContainer from "./components/MouseContainer";
+import CounterOne from "./components/CounterOne";
+import CounterTwo from "./components/CounterTwo";
+
+export const UserContext=React.createContext()
+export const ChannelContext=React.createContext()
 
 function App() {
   return <div className="App">
@@ -23,7 +29,16 @@ function App() {
     {/* <MouseContainer /> */}
 
     {/* <IntervalHookCounter/> */}
-    <DataFetching />
+    {/* <DataFetching /> */}
+    {/* <UserContext.Provider value={'rahul_jha'}>
+      <ChannelContext.Provider value={'Codeevolution'}>
+      <ComponentC />
+      </ChannelContext.Provider>
+   
+    </UserContext.Provider> */}
+
+    <CounterTwo/>
+    
 
   </div>;
 }
